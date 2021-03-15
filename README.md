5e6ed7072ac1ea3765fe2133aa220af436ce3cdd
landry <jlandry476@yahoo.fr>
2021-03-15 23:36:08 +0100

    After adding the video

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 "# TigerGraph-Streamli-Graphistry-WebApp" 
-"# TigerGraph-Streamli-Graphistry-WebApp" 
+<h1><b>Video Demo </b></h1>
+
+<iframe width="700" height="500" src="https://www.youtube.com/embed/_jkQ3AKXZ2o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
--- src/templates/home.py
+++ src/templates/home.py
@@ -15,9 +15,8 @@ def write():
 
         st.write("# International Medical Devices Database")
 
-        st.write('''
-                <iframe width="700" height="500" src="https://www.youtube.co">
-                </iframe>
+        st.write('''<h3><b>Video Demo </b></h3>
+                <iframe width="700" height="500" src="https://www.youtube.com/embed/_jkQ3AKXZ2o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             ''', 
             unsafe_allow_html=True)
 
